d7a7354c1026f1b27a18ff9e60f45bbb89c9445e
avlunen <avl1@gmx.de>
2026-04-25 21:23:45 +0100

    More clean-up

--- src/net/sf/jdivelog/gui/gasblending/GasOverflowEditWindow.java
+++ src/net/sf/jdivelog/gui/gasblending/GasOverflowEditWindow.java
@@ -22,191 +22,196 @@ import net.sf.jdivelog.model.gasoverflow.GasOverflowSource;
 
 public class GasOverflowEditWindow extends JDialog {
 
-	/**
-	 * 
-	 */
-	private JTextField tankdescription;
-	private JTextField tanksize;
-    private JTextField tankpressure;
-    private JPanel buttonPanel;    
-    private JButton closeButton;
-    private JButton cancelButton;
+   /**
+    * 
+    */
+   private JTextField tankdescription;
+   private JTextField tanksize;
+   private JTextField tankpressure;
+   private JPanel buttonPanel;
+   private JButton closeButton;
+   private JButton cancelButton;
 
-	private static final long serialVersionUID = 1L;
-	
-	private MainWindow mainWindow;
-	
-	private final GasOverflowTableModel gasSourceTableModel;
-    private final GasOverflowSource source;
+   private static final long serialVersionUID = 1L;
 
-	public GasOverflowEditWindow(Window parent, MainWindow mainWindow, GasOverflowTableModel gasSourceTableModel) {
-    	super(parent, ModalityType.APPLICATION_MODAL);
-    	this.mainWindow = mainWindow;
-        this.gasSourceTableModel = gasSourceTableModel;
-        this.source = null;
-        initialize();
-        load();
-        new MnemonicFactory(this);
-	}
-	
-    public GasOverflowEditWindow(Window parent, MainWindow mainWindow, GasOverflowTableModel gasSourceTableModel, GasOverflowSource source) {
-    	super(parent, ModalityType.APPLICATION_MODAL);
-    	this.mainWindow = mainWindow;
-        this.gasSourceTableModel = gasSourceTableModel;
-        this.source = source;
-        initialize();
-        load();
-    }
-	
-    private void initialize() {
-        setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/net/sf/jdivelog/gui/resources/icons/logo.gif")));
-        setTitle(Messages.getString("mixing.edit_gas_source")); //$NON-NLS-1$
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridBagLayout());
-        GridBagConstraints gc = new GridBagConstraints();
-        gc.anchor = GridBagConstraints.NORTHWEST;
-        gc.insets = new Insets(3,3,3,3);
-        gc.gridy = 0;
-        gc.gridx = 0;
-        panel.add(new JLabel(Messages.getString("overfill.gasoverflowsource.tankdescription")), gc); //$NON-NLS-1$
-        gc.gridx = 1;
-        panel.add(getTankDescription(), gc);
-        gc.gridy = 1;
-        gc.gridx = 0;
-        panel.add(new JLabel(Messages.getString("mixing.gasoverflowsource.tanksize")), gc); //$NON-NLS-1$
-        gc.gridx = 1;
-        panel.add(getTankSize(), gc);
-        gc.gridy = 2;
-        gc.gridx = 0;
-        panel.add(new JLabel(Messages.getString("mixing.gasoverflowsource.tankpressure")), gc); //$NON-NLS-1$
-        gc.gridx = 1;
-        panel.add(getTankPressure(), gc);
-        gc.gridy = 3;
-        gc.gridx = 0;
-        gc.gridwidth = 2;
-        panel.add(getButtonPanel(), gc);
-        setContentPane(panel);
-        pack();
-    }
-    
-    private JPanel getButtonPanel() {
-        if (buttonPanel == null) {
-            GridBagConstraints gc = new GridBagConstraints();
-            gc.fill = GridBagConstraints.HORIZONTAL;
-            gc.weightx = 0.5;
-            buttonPanel = new JPanel();
-            buttonPanel.setLayout(new GridBagLayout());
-            buttonPanel
-                    .setComponentOrientation(java.awt.ComponentOrientation.LEFT_TO_RIGHT);
-            gc.gridx = 0;
-            gc.gridy = 0;
-            gc.insets = new java.awt.Insets(5, 100, 5, 5);
-            buttonPanel.add(getCloseButton(), gc);
-            gc.gridx = 1;
-            gc.gridy = 0;
-            gc.insets = new java.awt.Insets(5, 5, 5, 100);
-            buttonPanel.add(getCancelButton(), gc);
-        }
-        return buttonPanel;
-    }
-    
-    private JButton getCloseButton() {
-        if (closeButton == null) {
-            closeButton = new JButton();
-            closeButton.setText(Messages.getString("close")); //$NON-NLS-1$
-            closeButton.addActionListener(new ActionListener() {
+   private MainWindow mainWindow;
 
-                public void actionPerformed(ActionEvent arg0) {
-                    close();
-                }
-                
-            });
-        }
-        return closeButton;
-    }
+   private final GasOverflowTableModel gasSourceTableModel;
+   private final GasOverflowSource source;
 
-    private JButton getCancelButton() {
-        if (cancelButton == null) {
-            cancelButton = new JButton();
-            cancelButton.setText(Messages.getString("cancel")); //$NON-NLS-1$
-            cancelButton.addActionListener(new ActionListener() {
+   public GasOverflowEditWindow(Window parent, MainWindow mainWindow, GasOverflowTableModel gasSourceTableModel) {
+      super(parent, ModalityType.APPLICATION_MODAL);
+      this.mainWindow = mainWindow;
+      this.gasSourceTableModel = gasSourceTableModel;
+      this.source = null;
+      initialize();
+      load();
+      new MnemonicFactory(this);
+   }
 
-                public void actionPerformed(ActionEvent arg0) {
-                    cancel();
-                }
-                
-            });
-        }
-        return cancelButton;
-    }
+   public GasOverflowEditWindow(Window parent, MainWindow mainWindow, GasOverflowTableModel gasSourceTableModel,
+         GasOverflowSource source) {
+      super(parent, ModalityType.APPLICATION_MODAL);
+      this.mainWindow = mainWindow;
+      this.gasSourceTableModel = gasSourceTableModel;
+      this.source = source;
+      initialize();
+      load();
+   }
 
-    private JTextField getTankDescription() {
-        if (tankdescription == null) {
-        	tankdescription = new JTextField();
-        	tankdescription.setColumns(20);
-        }
-        return tankdescription;
-    }
+   private void initialize() {
+      setIconImage(Toolkit.getDefaultToolkit()
+            .getImage(getClass().getResource("/net/sf/jdivelog/gui/resources/icons/logo.gif")));
+      setTitle(Messages.getString("mixing.edit_gas_source")); //$NON-NLS-1$
+      JPanel panel = new JPanel();
+      panel.setLayout(new GridBagLayout());
+      GridBagConstraints gc = new GridBagConstraints();
+      gc.anchor = GridBagConstraints.NORTHWEST;
+      gc.insets = new Insets(3, 3, 3, 3);
+      gc.gridy = 0;
+      gc.gridx = 0;
+      panel.add(new JLabel(Messages.getString("overfill.gasoverflowsource.tankdescription")), gc); //$NON-NLS-1$
+      gc.gridx = 1;
+      panel.add(getTankDescription(), gc);
+      gc.gridy = 1;
+      gc.gridx = 0;
+      panel.add(new JLabel(Messages.getString("mixing.gasoverflowsource.tanksize")), gc); //$NON-NLS-1$
+      gc.gridx = 1;
+      panel.add(getTankSize(), gc);
+      gc.gridy = 2;
+      gc.gridx = 0;
+      panel.add(new JLabel(Messages.getString("mixing.gasoverflowsource.tankpressure")), gc); //$NON-NLS-1$
+      gc.gridx = 1;
+      panel.add(getTankPressure(), gc);
+      gc.gridy = 3;
+      gc.gridx = 0;
+      gc.gridwidth = 2;
+      panel.add(getButtonPanel(), gc);
+      setContentPane(panel);
+      pack();
+   }
 
-    private JTextField getTankSize() {
-        if (tanksize == null) {
-        	tanksize = new JTextField();
-        	tanksize.setColumns(20);
-        }
-        return tanksize;
-    }
+   private JPanel getButtonPanel() {
+      if (buttonPanel == null) {
+         GridBagConstraints gc = new GridBagConstraints();
+         gc.fill = GridBagConstraints.HORIZONTAL;
+         gc.weightx = 0.5;
+         buttonPanel = new JPanel();
+         buttonPanel.setLayout(new GridBagLayout());
+         buttonPanel.setComponentOrientation(java.awt.ComponentOrientation.LEFT_TO_RIGHT);
+         gc.gridx = 0;
+         gc.gridy = 0;
+         gc.insets = new java.awt.Insets(5, 100, 5, 5);
+         buttonPanel.add(getCloseButton(), gc);
+         gc.gridx = 1;
+         gc.gridy = 0;
+         gc.insets = new java.awt.Insets(5, 5, 5, 100);
+         buttonPanel.add(getCancelButton(), gc);
+      }
+      return buttonPanel;
+   }
 
-    private JTextField getTankPressure() {
-        if (tankpressure == null) {
-        	tankpressure = new JTextField();
-        	tankpressure.setColumns(20);
-        }
-        return tankpressure;
-    }
-	
-    private void load() {
-        if (source == null) {
-            // new gas source
-            getTankDescription().setText("");
-            getTankSize().setText("50");
-            getTankPressure().setText("200");
-        } else {
-            // load data from existing source
-        	getTankDescription().setText(source.getTankdescription());
-        	getTankSize().setText(new Double(source.getTanksize()).toString());
-        	getTankPressure().setText(new Double(source.getTankpressure()).toString());
-        }
-    }
+   private JButton getCloseButton() {
+      if (closeButton == null) {
+         closeButton = new JButton();
+         closeButton.setText(Messages.getString("close")); //$NON-NLS-1$
+         closeButton.addActionListener(new ActionListener() {
 
-    public void close() {
-        if (save()) {
-            dispose();
-        }
-    }
-
-    public void cancel() {
-        dispose();
-    }
-    
-    private boolean save() {
-        try {
-            GasOverflowSource s = source;
-            if (s == null) {
-                s = new GasOverflowSource();
+            public void actionPerformed(ActionEvent arg0) {
+               close();
             }
-            s.setTankdescription(getTankDescription().getText());
-            s.setTankpressure(new Double(getTankPressure().getText()));
-            s.setTanksize(new Double(getTankSize().getText()));
-            if (source == null) {
-                gasSourceTableModel.addSource(s);
-            } else {
-                gasSourceTableModel.fireTableDataChanged();
+
+         });
+      }
+      return closeButton;
+   }
+
+   private JButton getCancelButton() {
+      if (cancelButton == null) {
+         cancelButton = new JButton();
+         cancelButton.setText(Messages.getString("cancel")); //$NON-NLS-1$
+         cancelButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent arg0) {
+               cancel();
             }
-        } catch (Exception e) {
-            new MessageDialog(this.mainWindow, Messages.getString("mixing.input_error"), Messages.getString("mixing.not_a_number"), null, MessageDialog.MessageType.ERROR); //$NON-NLS-1$  //$NON-NLS-2$
-            return false;
-        }
-        return true;
-    }
+
+         });
+      }
+      return cancelButton;
+   }
+
+   private JTextField getTankDescription() {
+      if (tankdescription == null) {
+         tankdescription = new JTextField();
+         tankdescription.setColumns(20);
+      }
+      return tankdescription;
+   }
+
+   private JTextField getTankSize() {
+      if (tanksize == null) {
+         tanksize = new JTextField();
+         tanksize.setColumns(20);
+      }
+      return tanksize;
+   }
+
+   private JTextField getTankPressure() {
+      if (tankpressure == null) {
+         tankpressure = new JTextField();
+         tankpressure.setColumns(20);
+      }
+      return tankpressure;
+   }
+
+   private void load() {
+      if (source == null) {
+         // new gas source
+         getTankDescription().setText("");
+         getTankSize().setText("50");
+         getTankPressure().setText("200");
+      }
+      else {
+         // load data from existing source
+         getTankDescription().setText(source.getTankdescription());
+         getTankSize().setText(Double.valueOf(source.getTanksize()).toString());
+         getTankPressure().setText(Double.valueOf(source.getTankpressure()).toString());
+      }
+   }
+
+   public void close() {
+      if (save()) {
+         dispose();
+      }
+   }
+
+   public void cancel() {
+      dispose();
+   }
+
+   private boolean save() {
+      try {
+         GasOverflowSource s = source;
+         if (s == null) {
+            s = new GasOverflowSource();
+         }
+         s.setTankdescription(getTankDescription().getText());
+         s.setTankpressure(Double.valueOf(getTankPressure().getText()));
+         s.setTanksize(Double.valueOf(getTankSize().getText()));
+         if (source == null) {
+            gasSourceTableModel.addSource(s);
+         }
+         else {
+            gasSourceTableModel.fireTableDataChanged();
+         }
+      }
+      catch (Exception e) {
+         new MessageDialog(this.mainWindow, Messages.getString("mixing.input_error"), //$NON-NLS-1$
+               Messages.getString("mixing.not_a_number"), null, MessageDialog.MessageType.ERROR); //$NON-NLS-1$
+         return false;
+      }
+      return true;
+   }
 
 }
